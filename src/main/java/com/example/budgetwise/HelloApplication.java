a4f6a561e728b99e8cff049dd345cd8abfa8fa75
Asad Bashir <ab442@myscc.ca>
2024-10-30 13:23:22 -0400

    #1 - Created structure of login page - added const.java to gitingore - started working on database class

--- src/main/java/com/example/budgetwise/HelloApplication.java
+++ src/main/java/com/example/budgetwise/HelloApplication.java
@@ -3,6 +3,13 @@ package com.example.budgetwise;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +17,38 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        BorderPane root = new BorderPane();
+        VBox userLoginBox = new VBox();
+
+        HBox userNameHBox = new HBox();
+        Label userNameLabel = new Label("Username");
+        TextField userNameTextField = new TextField();
+        userNameHBox.getChildren().addAll(userNameLabel,userNameTextField);
+
+
+        HBox passwordHBox = new HBox();
+        Label passwordLabel = new Label("Password");
+        PasswordField passwordField = new PasswordField();
+        passwordHBox.getChildren().addAll(passwordLabel,passwordField);
+
+
+        HBox serverHbox = new HBox();
+        Label serverLabel = new Label("Server Location");
+        TextField serverTextField = new TextField();
+        serverHbox.getChildren().addAll(serverLabel,serverTextField);
+
+        HBox databaseHBox = new HBox();
+        Label databaseLabel = new Label("Database Connection");
+        TextField databaseTextField = new TextField();
+        databaseHBox.getChildren().addAll(databaseLabel,databaseTextField);
+
+
+        Button testConnectionBtn = new Button("Test Connection");
+
+        userLoginBox.getChildren().addAll(userNameHBox,passwordHBox,serverHbox,databaseHBox,testConnectionBtn);
+
+        root.setCenter(userLoginBox);
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/example/budgetwise/database/Database.java
@@ -0,0 +1,14 @@
+package com.example.budgetwise.database;
+
+import java.sql.Connection;
+
+public class Database {
+
+    private static Database instance;
+    private Connection connection = null;
+
+    private Database(){
+
+
+    }
+}
