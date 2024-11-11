d042ba0300c2596ee8e58d5d1778d4cf43a4f036
Apekshya <w0842585@myscc.ca>
2024-11-11 16:11:41 -0500

    created input for account name and account type

--- src/main/java/com/example/budgetwise/Account.java
+++ src/main/java/com/example/budgetwise/Account.java
@@ -3,6 +3,9 @@ package com.example.budgetwise;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -15,6 +18,20 @@ public class Account extends Application {
         gridPane.setPadding(new Insets(10,10,10,10));
         gridPane.setVgap(10);
         gridPane.setHgap(10);
+
+        //creating label and textfield for account form
+        Label nameLabel = new Label("Account Name:");
+        TextField nameField = new TextField();
+        nameField.setPrefWidth(150);
+        gridPane.add(nameLabel,0,0);
+        gridPane.add(nameField,1,0);
+
+        //For Account type
+        Label accountLabel = new Label("Account Type:");
+        ComboBox<String> accountComboBox = new ComboBox<>();
+        accountComboBox.getItems().addAll("Checking", "Savings");
+        gridPane.add(accountLabel, 0, 1);
+        gridPane.add(accountComboBox, 1, 1);
 
 
 
