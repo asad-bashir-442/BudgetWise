c200596b58984bc6cc04062115c8d9db2dc1c705
Apekshya <w0842585@myscc.ca>
2024-11-11 17:21:54 -0500

    creating input for name and target amount for goals

--- src/main/java/com/example/budgetwise/Goals.java
+++ src/main/java/com/example/budgetwise/Goals.java
@@ -3,6 +3,8 @@ package com.example.budgetwise;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -15,11 +17,31 @@ public class Goals extends Application {
         gridPane.setVgap(10);
         gridPane.setHgap(10);
 
+        //creating input for Goal Name
+        Label nameLabel = new Label("Goal Name:");
+        TextField nameField = new TextField();
+        nameField.setPrefWidth(120);
+        gridPane.add(nameLabel,0,0);
+        gridPane.add(nameField,1,0);
+
+        //target amount
+        Label targetAmount = new Label("Target Amount:");
+        TextField amountField = new TextField();
+        amountField.setPrefWidth(120);
+        gridPane.add(targetAmount,0,1);
+        gridPane.add(amountField,1,1);
+
+
+
 
         Scene scene = new Scene(gridPane, 800,500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Account Form");
         primaryStage.show();
 
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
     }
 }
