4e3fcf8777ace42d0a700ae8eca825519d450259
Apekshya <w0842585@myscc.ca>
2024-11-11 16:07:26 -0500

    set the grid layout for the form and set it in the scene

---
+++ src/main/java/com/example/budgetwise/Account.java
@@ -0,0 +1,28 @@
+package com.example.budgetwise;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Account extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // grid pane Layout for the form
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10,10,10,10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+
+
+
+
+        Scene scene = new Scene(gridPane, 800,500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Account Form");
+        primaryStage.show();
+    }
+}
