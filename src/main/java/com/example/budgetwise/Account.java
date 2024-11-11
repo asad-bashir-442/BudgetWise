e1e5199ce1905382e6bf29b4a3a290d51721cf4a
Apekshya <w0842585@myscc.ca>
2024-11-11 16:29:26 -0500

    Added buttons to add account

--- src/main/java/com/example/budgetwise/Account.java
+++ src/main/java/com/example/budgetwise/Account.java
@@ -3,6 +3,7 @@ package com.example.budgetwise;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -51,6 +52,9 @@ public class Account extends Application {
         dateField.setPrefWidth(120);
         gridPane.add(dateLabel,0,4);
         gridPane.add(dateField,1,4);
+
+        Button button = new Button("Add Account +");
+        gridPane.add(button,1,6);
 
 
         Scene scene = new Scene(gridPane, 800,500);
