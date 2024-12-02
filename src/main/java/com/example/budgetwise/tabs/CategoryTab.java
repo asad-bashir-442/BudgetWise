910640db837e34656efb368c15c695ee42b0d778
Apekshya <W0842585@myscc.ca>
2024-12-02 00:41:28 -0500

    made some changes

--- src/main/java/com/example/budgetwise/tabs/CategoryTab.java
+++ src/main/java/com/example/budgetwise/tabs/CategoryTab.java
@@ -19,15 +19,23 @@ public class CategoryTab extends Tab {
         root.setPadding(new Insets(10,10,10,10));
         root.setVgap(10);
         root.setHgap(10);
+        root.getStyleClass().add("tab-background");
+
 
         Label nameLabel = new Label("Category Name:");
+        nameLabel.getStyleClass().add("label-style");
         TextField nameField = new TextField();
+        nameField.getStyleClass().add("textfield-style");
         nameField.setPrefWidth(150);
         root.add(nameLabel,0,0);
         root.add(nameField,1,0);
 
         Button button = new Button("Add category");
+        button.getStyleClass().add("button-style");
         root.add(button,1,2);
+
+        String css = getClass().getResource("/styles.css").toExternalForm();
+        root.getStylesheets().add(css);
 
         this.setContent(root);
         button.setOnAction(event ->{
